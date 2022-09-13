b9527798e1754cd9310cfbd74a5323895f485106
DevExpressExampleBot <fakeEmail>
2022-09-13 12:04:46 +0400

    Source auto update [skip ci]

--- VB/WindowsApplication53/Form1.Designer.vb
+++ VB/WindowsApplication53/Form1.Designer.vb
@@ -1,173 +1,182 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace WindowsApplication53
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.components = New System.ComponentModel.Container()
-			Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
-			Me.bindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
-			Me.dataSet1 = New System.Data.DataSet()
-			Me.dataTable1 = New System.Data.DataTable()
-			Me.dataColumn1 = New System.Data.DataColumn()
-			Me.dataColumn2 = New System.Data.DataColumn()
-			Me.dataColumn3 = New System.Data.DataColumn()
-			Me.dataColumn4 = New System.Data.DataColumn()
-			Me.fieldName = New DevExpress.XtraPivotGrid.PivotGridField()
-			Me.fieldYear = New DevExpress.XtraPivotGrid.PivotGridField()
-			Me.fieldValueX = New DevExpress.XtraPivotGrid.PivotGridField()
-			Me.fieldDate = New DevExpress.XtraPivotGrid.PivotGridField()
-			Me.fieldValueY = New DevExpress.XtraPivotGrid.PivotGridField()
-			CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.bindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.dataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.dataTable1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' pivotGridControl1
-			' 
-			Me.pivotGridControl1.Cursor = System.Windows.Forms.Cursors.Default
-			Me.pivotGridControl1.DataSource = Me.bindingSource1
-			Me.pivotGridControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.pivotGridControl1.Fields.AddRange(New DevExpress.XtraPivotGrid.PivotGridField() { Me.fieldName, Me.fieldYear, Me.fieldValueX, Me.fieldDate, Me.fieldValueY})
-			Me.pivotGridControl1.Location = New System.Drawing.Point(0, 0)
-			Me.pivotGridControl1.Name = "pivotGridControl1"
-			Me.pivotGridControl1.OptionsView.ColumnTotalsLocation = DevExpress.XtraPivotGrid.PivotTotalsLocation.Near
-			Me.pivotGridControl1.Size = New System.Drawing.Size(796, 292)
-			Me.pivotGridControl1.TabIndex = 0
-'			Me.pivotGridControl1.CustomAppearance += New DevExpress.XtraPivotGrid.PivotCustomAppearanceEventHandler(Me.pivotGridControl1_CustomAppearance);
-			' 
-			' bindingSource1
-			' 
-			Me.bindingSource1.DataMember = "Data"
-			Me.bindingSource1.DataSource = Me.dataSet1
-			' 
-			' dataSet1
-			' 
-			Me.dataSet1.DataSetName = "NewDataSet"
-			Me.dataSet1.Tables.AddRange(New System.Data.DataTable() { Me.dataTable1})
-			' 
-			' dataTable1
-			' 
-			Me.dataTable1.Columns.AddRange(New System.Data.DataColumn() { Me.dataColumn1, Me.dataColumn2, Me.dataColumn3, Me.dataColumn4})
-			Me.dataTable1.TableName = "Data"
-			' 
-			' dataColumn1
-			' 
-			Me.dataColumn1.ColumnName = "Name"
-			' 
-			' dataColumn2
-			' 
-			Me.dataColumn2.ColumnName = "Date"
-			Me.dataColumn2.DataType = GetType(System.DateTime)
-			' 
-			' dataColumn3
-			' 
-			Me.dataColumn3.ColumnName = "ValueX"
-			Me.dataColumn3.DataType = GetType(Decimal)
-			' 
-			' dataColumn4
-			' 
-			Me.dataColumn4.ColumnName = "ValueY"
-			Me.dataColumn4.DataType = GetType(Decimal)
-			' 
-			' fieldName
-			' 
-			Me.fieldName.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea
-			Me.fieldName.AreaIndex = 0
-			Me.fieldName.FieldName = "Name"
-			Me.fieldName.Name = "fieldName"
-			' 
-			' fieldYear
-			' 
-			Me.fieldYear.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea
-			Me.fieldYear.AreaIndex = 0
-			Me.fieldYear.Caption = "Year"
-			Me.fieldYear.FieldName = "Date"
-			Me.fieldYear.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.DateYear
-			Me.fieldYear.Name = "fieldYear"
-			Me.fieldYear.UnboundFieldName = "fieldDate"
-			' 
-			' fieldValueX
-			' 
-			Me.fieldValueX.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
-			Me.fieldValueX.AreaIndex = 0
-			Me.fieldValueX.Caption = "X"
-			Me.fieldValueX.FieldName = "ValueX"
-			Me.fieldValueX.Name = "fieldValueX"
-			' 
-			' fieldDate
-			' 
-			Me.fieldDate.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea
-			Me.fieldDate.AreaIndex = 1
-			Me.fieldDate.Caption = "Date"
-			Me.fieldDate.FieldName = "Date"
-			Me.fieldDate.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.Date
-			Me.fieldDate.Name = "fieldDate"
-			Me.fieldDate.UnboundFieldName = "fieldDate1"
-			' 
-			' fieldValueY
-			' 
-			Me.fieldValueY.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
-			Me.fieldValueY.AreaIndex = 1
-			Me.fieldValueY.Caption = "Y"
-			Me.fieldValueY.FieldName = "ValueY"
-			Me.fieldValueY.Name = "fieldValueY"
-			Me.fieldValueY.Options.ShowGrandTotal = False
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(796, 292)
-			Me.Controls.Add(Me.pivotGridControl1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-'			Me.Load += New System.EventHandler(Me.Form1_Load);
-			CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.bindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.dataSet1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.dataTable1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
+            Me.bindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+            Me.dataSet1 = New System.Data.DataSet()
+            Me.dataTable1 = New System.Data.DataTable()
+            Me.dataColumn1 = New System.Data.DataColumn()
+            Me.dataColumn2 = New System.Data.DataColumn()
+            Me.dataColumn3 = New System.Data.DataColumn()
+            Me.dataColumn4 = New System.Data.DataColumn()
+            Me.fieldName = New DevExpress.XtraPivotGrid.PivotGridField()
+            Me.fieldYear = New DevExpress.XtraPivotGrid.PivotGridField()
+            Me.fieldValueX = New DevExpress.XtraPivotGrid.PivotGridField()
+            Me.fieldDate = New DevExpress.XtraPivotGrid.PivotGridField()
+            Me.fieldValueY = New DevExpress.XtraPivotGrid.PivotGridField()
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.bindingSource1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.dataSet1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.dataTable1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' pivotGridControl1
+            ' 
+            Me.pivotGridControl1.Cursor = System.Windows.Forms.Cursors.[Default]
+            Me.pivotGridControl1.DataSource = Me.bindingSource1
+            Me.pivotGridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.pivotGridControl1.Fields.AddRange(New DevExpress.XtraPivotGrid.PivotGridField() {Me.fieldName, Me.fieldYear, Me.fieldValueX, Me.fieldDate, Me.fieldValueY})
+            Me.pivotGridControl1.Location = New System.Drawing.Point(0, 0)
+            Me.pivotGridControl1.Name = "pivotGridControl1"
+            Me.pivotGridControl1.OptionsView.ColumnTotalsLocation = DevExpress.XtraPivotGrid.PivotTotalsLocation.Near
+            Me.pivotGridControl1.Size = New System.Drawing.Size(796, 292)
+            Me.pivotGridControl1.TabIndex = 0
+            AddHandler Me.pivotGridControl1.CustomAppearance, New DevExpress.XtraPivotGrid.PivotCustomAppearanceEventHandler(AddressOf Me.pivotGridControl1_CustomAppearance)
+            ' 
+            ' bindingSource1
+            ' 
+            Me.bindingSource1.DataMember = "Data"
+            Me.bindingSource1.DataSource = Me.dataSet1
+            ' 
+            ' dataSet1
+            ' 
+            Me.dataSet1.DataSetName = "NewDataSet"
+            Me.dataSet1.Tables.AddRange(New System.Data.DataTable() {Me.dataTable1})
+            ' 
+            ' dataTable1
+            ' 
+            Me.dataTable1.Columns.AddRange(New System.Data.DataColumn() {Me.dataColumn1, Me.dataColumn2, Me.dataColumn3, Me.dataColumn4})
+            Me.dataTable1.TableName = "Data"
+            ' 
+            ' dataColumn1
+            ' 
+            Me.dataColumn1.ColumnName = "Name"
+            ' 
+            ' dataColumn2
+            ' 
+            Me.dataColumn2.ColumnName = "Date"
+            Me.dataColumn2.DataType = GetType(System.DateTime)
+            ' 
+            ' dataColumn3
+            ' 
+            Me.dataColumn3.ColumnName = "ValueX"
+            Me.dataColumn3.DataType = GetType(Decimal)
+            ' 
+            ' dataColumn4
+            ' 
+            Me.dataColumn4.ColumnName = "ValueY"
+            Me.dataColumn4.DataType = GetType(Decimal)
+            ' 
+            ' fieldName
+            ' 
+            Me.fieldName.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea
+            Me.fieldName.AreaIndex = 0
+            Me.fieldName.FieldName = "Name"
+            Me.fieldName.Name = "fieldName"
+            ' 
+            ' fieldYear
+            ' 
+            Me.fieldYear.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea
+            Me.fieldYear.AreaIndex = 0
+            Me.fieldYear.Caption = "Year"
+            Me.fieldYear.FieldName = "Date"
+            Me.fieldYear.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.DateYear
+            Me.fieldYear.Name = "fieldYear"
+            Me.fieldYear.UnboundFieldName = "fieldDate"
+            ' 
+            ' fieldValueX
+            ' 
+            Me.fieldValueX.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
+            Me.fieldValueX.AreaIndex = 0
+            Me.fieldValueX.Caption = "X"
+            Me.fieldValueX.FieldName = "ValueX"
+            Me.fieldValueX.Name = "fieldValueX"
+            ' 
+            ' fieldDate
+            ' 
+            Me.fieldDate.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea
+            Me.fieldDate.AreaIndex = 1
+            Me.fieldDate.Caption = "Date"
+            Me.fieldDate.FieldName = "Date"
+            Me.fieldDate.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.[Date]
+            Me.fieldDate.Name = "fieldDate"
+            Me.fieldDate.UnboundFieldName = "fieldDate1"
+            ' 
+            ' fieldValueY
+            ' 
+            Me.fieldValueY.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
+            Me.fieldValueY.AreaIndex = 1
+            Me.fieldValueY.Caption = "Y"
+            Me.fieldValueY.FieldName = "ValueY"
+            Me.fieldValueY.Name = "fieldValueY"
+            Me.fieldValueY.Options.ShowGrandTotal = False
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(796, 292)
+            Me.Controls.Add(Me.pivotGridControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.bindingSource1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.dataSet1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.dataTable1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private WithEvents pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
-		Private dataSet1 As System.Data.DataSet
-		Private dataTable1 As System.Data.DataTable
-		Private dataColumn1 As System.Data.DataColumn
-		Private dataColumn2 As System.Data.DataColumn
-		Private dataColumn3 As System.Data.DataColumn
-		Private fieldName As DevExpress.XtraPivotGrid.PivotGridField
-		Private fieldYear As DevExpress.XtraPivotGrid.PivotGridField
-		Private fieldValueX As DevExpress.XtraPivotGrid.PivotGridField
-		Private fieldDate As DevExpress.XtraPivotGrid.PivotGridField
-		Private dataColumn4 As System.Data.DataColumn
-		Private fieldValueY As DevExpress.XtraPivotGrid.PivotGridField
-		Private bindingSource1 As System.Windows.Forms.BindingSource
-	End Class
+#End Region
+        Private pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
+
+        Private dataSet1 As System.Data.DataSet
+
+        Private dataTable1 As System.Data.DataTable
+
+        Private dataColumn1 As System.Data.DataColumn
+
+        Private dataColumn2 As System.Data.DataColumn
+
+        Private dataColumn3 As System.Data.DataColumn
+
+        Private fieldName As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private fieldYear As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private fieldValueX As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private fieldDate As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private dataColumn4 As System.Data.DataColumn
+
+        Private fieldValueY As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private bindingSource1 As System.Windows.Forms.BindingSource
+    End Class
 End Namespace
-
--- VB/WindowsApplication53/Form1.vb
+++ VB/WindowsApplication53/Form1.vb
@@ -1,81 +1,77 @@
-Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.XtraPivotGrid
 
 Namespace WindowsApplication53
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			PopulateTable()
-			pivotGridControl1.RefreshData()
-			pivotGridControl1.BestFit()
-		End Sub
-		Private Sub PopulateTable()
-			Dim myTable As DataTable = dataSet1.Tables("Data")
-			myTable.Rows.Add(New Object() { "Aaa", DateTime.Today, 7, 3 })
-			myTable.Rows.Add(New Object() { "Aaa", DateTime.Today.AddDays(1), 4, 1 })
-			myTable.Rows.Add(New Object() { "Bbb", DateTime.Today, 12, 6 })
-			myTable.Rows.Add(New Object() { "Bbb", DateTime.Today.AddDays(1), 14, 7 })
-			myTable.Rows.Add(New Object() { "Ccc", DateTime.Today, 11, 1 })
-			myTable.Rows.Add(New Object() { "Ccc", DateTime.Today.AddDays(1), 10, 3 })
 
-			myTable.Rows.Add(New Object() { "Aaa", DateTime.Today.AddYears(1), 4, 5 })
-			myTable.Rows.Add(New Object() { "Aaa", DateTime.Today.AddYears(1).AddDays(1), 2, 8 })
-			myTable.Rows.Add(New Object() { "Bbb", DateTime.Today.AddYears(1), 3, 9 })
-			myTable.Rows.Add(New Object() { "Bbb", DateTime.Today.AddDays(1).AddYears(1), 1, 5 })
-			myTable.Rows.Add(New Object() { "Ccc", DateTime.Today.AddYears(1), 8, 3 })
-			myTable.Rows.Add(New Object() { "Ccc", DateTime.Today.AddDays(1).AddYears(1), 22 })
-		End Sub
+    Public Partial Class Form1
+        Inherits Form
 
-		Private Sub pivotGridControl1_CustomAppearance(ByVal sender As Object, ByVal e As DevExpress.XtraPivotGrid.PivotCustomAppearanceEventArgs) Handles pivotGridControl1.CustomAppearance
-			Dim groupIndex As Integer = GetGroupIndex(CType(sender, PivotGridControl), e)
-			If groupIndex Mod 2 = 0 Then
-				e.Appearance.BackColor = Color.GreenYellow
-			End If
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		Private Function GetGroupIndex(ByVal pivot As PivotGridControl, ByVal e As PivotCustomAppearanceEventArgs) As Integer
-			Dim columnFields() As PivotGridField = pivot.GetFieldsByArea(PivotArea.ColumnArea).ToArray()
-			Dim groupIndex As Integer = -1
-			Dim previous(columnFields.Length - 1) As Object
-			Dim current(columnFields.Length - 1) As Object
-			GetValuesByIndex(pivot, columnFields, previous, 0)
-			For i As Integer = 1 To e.ColumnIndex
-				GetValuesByIndex(pivot, columnFields, current, i)
-				If (Not ValuesEquals(previous,current)) Then
-					groupIndex += 1
-					current.CopyTo(previous, 0)
-				End If
-			Next i
-			Dim o As New List(Of Object)()
-			Return groupIndex
-		End Function
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            PopulateTable()
+            pivotGridControl1.RefreshData()
+            pivotGridControl1.BestFit()
+        End Sub
 
-		Private Function ValuesEquals(ByVal previous() As Object, ByVal current() As Object) As Boolean
-			For i As Integer = 0 To previous.Length - 1
-				If (Not Equals(previous(i), current(i))) Then
-					Return False
-				End If
-			Next i
-			Return True
-		End Function
+        Private Sub PopulateTable()
+            Dim myTable As DataTable = dataSet1.Tables("Data")
+            myTable.Rows.Add(New Object() {"Aaa", Date.Today, 7, 3})
+            myTable.Rows.Add(New Object() {"Aaa", Date.Today.AddDays(1), 4, 1})
+            myTable.Rows.Add(New Object() {"Bbb", Date.Today, 12, 6})
+            myTable.Rows.Add(New Object() {"Bbb", Date.Today.AddDays(1), 14, 7})
+            myTable.Rows.Add(New Object() {"Ccc", Date.Today, 11, 1})
+            myTable.Rows.Add(New Object() {"Ccc", Date.Today.AddDays(1), 10, 3})
+            myTable.Rows.Add(New Object() {"Aaa", Date.Today.AddYears(1), 4, 5})
+            myTable.Rows.Add(New Object() {"Aaa", Date.Today.AddYears(1).AddDays(1), 2, 8})
+            myTable.Rows.Add(New Object() {"Bbb", Date.Today.AddYears(1), 3, 9})
+            myTable.Rows.Add(New Object() {"Bbb", Date.Today.AddDays(1).AddYears(1), 1, 5})
+            myTable.Rows.Add(New Object() {"Ccc", Date.Today.AddYears(1), 8, 3})
+            myTable.Rows.Add(New Object() {"Ccc", Date.Today.AddDays(1).AddYears(1), 22})
+        End Sub
 
-		Private Sub GetValuesByIndex(ByVal pivot As PivotGridControl, ByVal fields() As PivotGridField, ByVal current() As Object, ByVal index As Integer)
-			For i As Integer = 0 To fields.Length - 1
-				current(i) = pivot.GetFieldValue(fields(i), index)
-			Next i
-		End Sub
+        Private Sub pivotGridControl1_CustomAppearance(ByVal sender As Object, ByVal e As PivotCustomAppearanceEventArgs)
+            Dim groupIndex As Integer = GetGroupIndex(CType(sender, PivotGridControl), e)
+            If groupIndex Mod 2 = 0 Then e.Appearance.BackColor = Color.GreenYellow
+        End Sub
 
+        Private Function GetGroupIndex(ByVal pivot As PivotGridControl, ByVal e As PivotCustomAppearanceEventArgs) As Integer
+            Dim columnFields As PivotGridField() = pivot.GetFieldsByArea(PivotArea.ColumnArea).ToArray()
+            Dim groupIndex As Integer = -1
+            Dim previous As Object() = New Object(columnFields.Length - 1) {}
+            Dim current As Object() = New Object(columnFields.Length - 1) {}
+            GetValuesByIndex(pivot, columnFields, previous, 0)
+            For i As Integer = 1 To e.ColumnIndex
+                GetValuesByIndex(pivot, columnFields, current, i)
+                If Not ValuesEquals(previous, current) Then
+                    groupIndex += 1
+                    current.CopyTo(previous, 0)
+                End If
+            Next
 
+            Dim o As List(Of Object) = New List(Of Object)()
+            Return groupIndex
+        End Function
 
-	End Class
+        Private Function ValuesEquals(ByVal previous As Object(), ByVal current As Object()) As Boolean
+            For i As Integer = 0 To previous.Length - 1
+                If Not Equals(previous(i), current(i)) Then Return False
+            Next
+
+            Return True
+        End Function
+
+        Private Sub GetValuesByIndex(ByVal pivot As PivotGridControl, ByVal fields As PivotGridField(), ByVal current As Object(), ByVal index As Integer)
+            For i As Integer = 0 To fields.Length - 1
+                current(i) = pivot.GetFieldValue(fields(i), index)
+            Next
+        End Sub
+    End Class
 End Namespace
